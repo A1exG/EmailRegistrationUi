f46b68570ed7be8dc88e09179d47a160a53c44af
Алексей Гагарин <alex4619748@rambler.ru>
2020-05-24 13:52:52 +0300

    Добавьте файлы проекта.

---
+++ EmailRegistrationUi/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using EmailRegistrationUi.Views;
+using System.Windows;
+
+namespace EmailRegistrationUi
+{
+    public partial class App : Application
+    {
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			MainWindowView mwv = new MainWindowView();
+			mwv.Show();
+			mwv.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+			Application.Current.MainWindow.Closed += (s, a) =>
+			{
+				Shutdown();
+			};
+		}
+	}
+}
---
+++ EmailRegistrationUi/Views/MainWindowView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EmailRegistrationUi.Views
+{
+    public partial class MainWindowView : Window
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+
+            btnAddNewEmail.Click += (s, e) =>
+            {
+                AddNewEmailView anev = new AddNewEmailView();
+                anev.Owner = this;
+                anev.ShowDialog();
+            };
+
+            btnShowAllEmail.Click += (s, e) =>
+            {
+                ShowAllEmailView saev = new ShowAllEmailView();
+                saev.Owner = this;
+                saev.ShowDialog();
+            };
+
+            btnShowEmailInId.Click += (s, e) =>
+            {
+                ShowEmailInIdView seiv = new ShowEmailInIdView();
+                seiv.Owner = this;
+                seiv.ShowDialog();
+            };
+        }
+    }
+}
